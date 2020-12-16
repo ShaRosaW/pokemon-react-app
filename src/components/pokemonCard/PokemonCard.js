9fc58d371bd4c5ff56d7ce0c7099a10844723c80
sharonrosa <srwijnberg@gmail.com>
2020-12-16 13:26:32 +0100

    characteristics pokemon, loading and error

--- src/components/pokemonCard/PokemonCard.js
+++ src/components/pokemonCard/PokemonCard.js
@@ -2,28 +2,70 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './PokemonCard.css';
 
-export const PokemonCard = ({name,url}) => {
+export const PokemonCard = ({url}) => {
     const [pokemonData, setPokemonData] = useState(null);
+    const [status, setStatus] = useState("loading");
 
     useEffect(() => {
         const getPokemonData = async () => {
             try {
                 const response = await axios.get(`${url}`);
+                console.log(response.data);
                 setPokemonData(response.data);
+                setStatus("done loading")
             }
             catch (e) {
-                console.log(e)
+                // alert("Oops, the pokemons got away");
+                setStatus("error");
+                //console.log(e)
             }
         }
         getPokemonData();
     }, [url]);
 
-    return (
+    if (status === "loading") {
+        return <h3>Loading</h3>;
+    }   else if (status === "error") {
+        return <h3>O nooo, the pokemons have disappeared, click refresh</h3>;
+    }   else {
+
+
+        return (
         <>
-            {pokemonData && <div>
-                <h3>{name}</h3>
-            </div>}
+            {pokemonData ? (<div>
+                <ul>
+                    <li><h2>{pokemonData.name}</h2></li>
+                    <li className="image">
+                    <h3><img src={pokemonData.sprites.front_default}
+                             alt={pokemonData.name}
+                    />
+                    </h3>
+                    </li>
+                    <li>
+                        <h4>Moves:</h4>
+                        {pokemonData.moves.length}
+                    </li>
+                    <li>
+                        <h4>Weight:</h4>
+                        {pokemonData.weight}
+                    </li>
+                </ul>
+                <ul className="abilities">
+                    <li>
+                        <h4>Abilities:</h4>
+                    </li>
+                        {pokemonData.abilities.map((ability) => {
+                            return <li key={ability.ability.name}>
+                                    {ability.ability.name}</li>
+                })}
+
+                </ul>
+            </div>
+            ) : (
+                <span className="loading">Loading...</span>
+                )
+            }
         </>
 
     )
-}
+}}
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -7,15 +7,18 @@ export const Homepage = () => {
     const [pokemonData, setPokemonData] = useState(null);
     const [offset, setOffset] = useState(0);
 
+
     useEffect(() => {
         const getPokemons = async () => {
             try {
-                const response = await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=${offset}`);
-                console.log(response.data.results)
+                const response = await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=20&offset=${offset}`);
+                //console.log(response.data)
                 setPokemonData(response.data.results);
+
             }
             catch (e) {
-                console.log(e)
+                //console.log(e)
+
             }
         }
         getPokemons();
@@ -23,23 +26,29 @@ export const Homepage = () => {
 
     const getNext = () => {
         console.log("getNext");
-        setOffset(offset + 10);
+        setOffset(offset + 20);
     }
     const getPrevious = () => {
         console.log("getPrevious");
-        if ( offset !== 0 ) setOffset(offset - 10);
+        if ( offset !== 0 ) setOffset(offset - 20);
     }
 
     return (
         <>
-            <h1> Homepage Pokemon </h1>
+            <header>
+                <h1>Pokemon, gotta catch 'em all!!</h1>
+            </header>
             <div>
                 <NavButtons onNext={getNext} onPrevious={getPrevious}/>
             </div>
             <div>
                 {pokemonData && (
                     pokemonData.map(({name, url}) => {
-                        return <PokemonCard key={name} name={name} url={url}/>
+                        return <PokemonCard
+                            key={name}
+                            url={url}
+
+                        />
                     })
                 )}
             </div>
